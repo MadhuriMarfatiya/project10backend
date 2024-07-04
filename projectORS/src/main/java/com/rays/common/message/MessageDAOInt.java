7abae34521e44b12f01d4c20a86c01540943289c
MadhuriMarfatiya <madhurirami19@gmail.com>
2024-07-04 16:03:58 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Deepraj Chouhan 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ * @author Deepraj Chouhan 
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
